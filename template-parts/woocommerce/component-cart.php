b691d43fc95dcac661a5aa1461b2d6bf68e811e3
welldecode <wellingtonalfredo550@gmail.com>
2023-06-29 20:33:05 -0300

    adicionado arquivos

--- template-parts/woocommerce/component-cart.php
+++ template-parts/woocommerce/component-cart.php
@@ -2,37 +2,32 @@
     <div class="cart_block_content">
         <div class="topo_block_content">
             <div class="title_block_cart">
-                <h1>Carrinho de Compras</h1> 
+                <h1>Carrinho de Compras</h1>
                 <img src="<?= THEME_URI ?>/assets/img/woocommerce/close-circle-line.svg" alt="">
             </div>
             <?php
-    global $woocommerce;
-    $items = $woocommerce->cart->get_cart();
+            global $woocommerce;
+            $items = $woocommerce->cart->get_cart();
 
-        foreach($items as $item => $values) { 
-            $_product =  wc_get_product( $values['data']->get_id() );
-            //product image
-            $getProductDetail = wc_get_product( $values['product_id'] );
-     
-            echo "<b>".$_product->get_title() .'</b>  <br> Quantity: '.$values['quantity'].'<br>'; 
-            $price = get_post_meta($values['product_id'] , '_price', true);
-            echo "  Price: ".$price."<br>";
-            /*Regular Price and Sale Price*/
-            echo "Regular Price: ".get_post_meta($values['product_id'] , '_regular_price', true)."<br>";
-            echo "Sale Price: ".get_post_meta($values['product_id'] , '_sale_price', true)."<br>";
-        }
-?>
-            <div class="cart_block_item">
-                <a href="#">
-                    <div class="info_item">
-                        <h2>Item</h2>
-                        <span>Descrição</span>
-                    </div>
-                    <div class="info_price_item">
-                        R$ 00,00
-                    </div>
-                </a>
-            </div>
+            foreach ($items as $item => $values) { 
+                $_product = wc_get_product( $values['data']->get_id() );
+            ?>
+
+                <div class="cart_block_item">
+                    <a href="#">
+                        <div class="info_item">
+                            <h2><?php echo $_product->get_title(); ?></h2>
+                            <span>Descrição</span>
+                        </div>
+                        <div class="info_price_item">
+                            <?php echo get_post_meta($values['product_id'] , '_price', true); ?>
+                        </div>
+                    </a>
+                </div>
+             
+            <?php
+            }
+            ?>
         </div>
         <div class="footer_cart">
             <a href="#" class="btn_continue">Continuar Comprando</a>
