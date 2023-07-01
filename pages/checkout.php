c85c3965bae8d03a8970abe18b7e342078437852
welldecode <wellingtonalfredo550@gmail.com>
2023-07-01 18:05:35 -0300

    adicionado arquivos

--- pages/checkout.php
+++ pages/checkout.php
@@ -24,33 +24,63 @@
 
                 <ul class="breadcrumb">
                     <li>Carrinho</li>
-                    <li>Carrinho</li>
-                    <li>Carrinho</li>
+                    <li>Informações & Frete</li>
+                    <li>Pagamento</li>
                 </ul>
-                <form name="checkout" method="post" class="checkout woocommerce-checkout" action="http://localhost/temas_wordpress/katabarba/index.php/checkout/" enctype="multipart/form-data">
-                    <div id="wc-stripe-payment-request-wrapper" style="clear:both;padding-top:1.5em;display:none;">
-                        <div id="wc-stripe-payment-request-button"></div>
-                    </div>
-                    <div class="col2-set" id="customer_details">
-<div class="col-1">
-<div class="woocommerce-billing-fields">
-<h3>Detalhes de faturamento</h3>
-<div class="woocommerce-billing-fields__field-wrapper">
-</div>
-</div>
-</div>
-<div class="col-2">
-<div class="woocommerce-shipping-fields">
-</div>
-<div class="woocommerce-additional-fields">
-<h3>Informação adicional</h3>
-<div class="woocommerce-additional-fields__field-wrapper">
-</div>
-</div>
-</div>
-</div>
-                    <button type="submit" class="button alt" name="woocommerce_checkout_place_order" id="place_order" value="Finalizar pedido" data-value="Finalizar pedido">Finalizar pedido</button>
 
+                <span class="title_order">Informações</span>
+                <form action="#">
+                    <div class="input_group">
+                        <input type="email" placeholder="Seu E-mail">
+                    </div>
+                    <div class="input_list_group">
+                        <div class="input_group">
+                            <input type="text" placeholder="Nome">
+                        </div>
+                        <div class="input_group">
+                            <input type="email" placeholder="Sobrenome">
+                        </div>
+
+                        <div class="input_group">
+                            <input type="email" placeholder="CPF">
+                        </div>
+
+                        <div class="input_group">
+                            <input type="number" placeholder="Telefone">
+                        </div>
+                    </div> <br>
+
+                    <span class="title_order">Endereço de entrega</span>
+                    <div class="input_group">
+                        <input type="number" placeholder="CEP">
+                    </div>
+                    <div class="input_list_group">
+                        <div class="input_group" style="width: 67%;">
+                            <input type="text" placeholder="Endereço">
+                        </div>
+                        <div class="input_group" style="width: 30%;">
+                            <input type="number" placeholder="Numero">
+                        </div>
+
+                        <div class="input_group" style="width: 52%;">
+                            <input type="text" placeholder="Bairro">
+                        </div>
+
+                        <div class="input_group" style="width: 45%;">
+                            <input type="text" placeholder="Complemento">
+                        </div>
+                        <div class="input_group">
+                            <input type="text" placeholder="Cidade">
+                        </div>
+
+                        <div class="input_group">
+                            <input type="text" placeholder="Estado">
+                        </div>
+                    </div>
+                    <div class="footer_button">
+                        <a href="#">Voltar ao Carrinho</a>
+                        <a href="#">Ir para Pagamento</a>
+                    </div>
                 </form>
             </div>
             <div class="checkout_items">
@@ -83,14 +113,38 @@
 
                             </div>
                         </div>
-
-                </div>
-
-            <?php
+                    <?php
                     }
-            ?>
+                    ?>
+                    <hr>
+
+                    <div class="cupom_content">
+                        <h1>Tem um cupom de desconto?</h1>
+                        <div class="input_cupom">
+                            <input type="text" placeholder="cartão-presente ou código de desconto">
+                            <a href="#">Aplicar</a>
+                        </div>
+                    </div>
+
+                    <hr>
+                    <div class="price_total">
+                        <div class="left_price">
+                            <div class="col-1">
+                                <span>SubTotal</span>
+                                <span>R$ <?php echo  WC()->cart->get_subtotal(); ?></span>
+                            </div> 
+                            <div class="col-1">
+                                <span>Frete</span>
+                                <span>R$ <?php echo WC()->cart->get_subtotal_tax(); ?></span>
+                            </div>
+                        </div>
+                    </div>
+                    <hr>
+                    <div class="total_order">
+                     <?php echo WC()->cart->get_cart_subtotal(); ?>  
+                    </div>
+                </div>
             </div>
-        </div>
         </div>
     </section>
 </main>
@@ -99,7 +153,6 @@
     const base_url = '<?php echo get_site_url(); ?>';
 </script>
 
-<?php get_template_part('template-parts/woocommerce/component', 'cart'); ?>
 <?php wp_footer(); ?>
 
 </body>
