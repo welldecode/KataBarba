ce9056c61eb910fda35dfc40ef02fc851c58f704
welldecode <wellingtonalfredo550@gmail.com>
2023-07-05 18:01:25 -0300

    adicionado arquivos

--- pages/checkout.php
+++ pages/checkout.php
@@ -149,7 +149,46 @@
         </div>
     </section>
 </main>
-<div id="payment"> 
+
+<form id="order_review" method="post">
+
+	<table class="shop_table">
+		<thead>
+			<tr>
+				<th class="product-name"><?php _e( 'Product', 'woocommerce' ); ?></th>
+				<th class="product-quantity"><?php _e( 'Qty', 'woocommerce' ); ?></th>
+				<th class="product-total"><?php _e( 'Totals', 'woocommerce' ); ?></th>
+			</tr>
+		</thead>
+		<tbody>
+			<?php
+			if ( sizeof( $order->get_items() ) > 0 ) :
+				foreach ( $order->get_items() as $item ) :
+					echo '
+						<tr>
+							<td class="product-name">' . $item['name'].'</td>
+							<td class="product-quantity">' . $item['qty'].'</td>
+							<td class="product-subtotal">' . $order->get_formatted_line_subtotal( $item ) . '</td>
+						</tr>';
+				endforeach;
+			endif;
+			?>
+		</tbody>
+		<tfoot>
+		<?php
+			if ( $totals = $order->get_order_item_totals() ) foreach ( $totals as $total ) :
+				?>
+				<tr>
+					<th scope="row" colspan="2"><?php echo $total['label']; ?></th>
+					<td class="product-total"><?php echo $total['value']; ?></td>
+				</tr>
+				<?php
+			endforeach;
+		?>
+		</tfoot>
+	</table>
+
+	<div id="payment"> 
 		<ul class="payment_methods methods">
 			<?php
 				if ( $available_gateways = WC()->payment_gateways->get_available_payment_gateways() ) {
@@ -180,7 +219,6 @@
 				}
 			?>
 		</ul> 
-
 		<div class="form-row">
 			<?php wp_nonce_field( 'woocommerce-pay' ); ?>
 			<?php
@@ -193,6 +231,7 @@
 
 	</div>
 
+</form>
 <script>
     const base_url = '<?php echo get_site_url(); ?>';
 </script>
