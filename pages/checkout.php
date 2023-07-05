924c0735d5c1f474f1bd7839d55b15810c5d19ab
welldecode <wellingtonalfredo550@gmail.com>
2023-07-05 18:02:29 -0300

    adicionado arquivos

--- pages/checkout.php
+++ pages/checkout.php
@@ -195,7 +195,18 @@
 		</div>
 
 	</div>
+    <li class="payment_method_<?php echo $gateway->id; ?>">
+	<input id="payment_method_<?php echo $gateway->id; ?>" type="radio" class="input-radio" name="payment_method" value="<?php echo esc_attr( $gateway->id ); ?>" <?php checked( $gateway->chosen, true ); ?> data-order_button_text="<?php echo esc_attr( $gateway->order_button_text ); ?>" />
 
+	<label for="payment_method_<?php echo $gateway->id; ?>">
+		<?php echo $gateway->get_title(); ?> <?php echo $gateway->get_icon(); ?>
+	</label>
+	<?php if ( $gateway->has_fields() || $gateway->get_description() ) : ?>
+		<div class="payment_box payment_method_<?php echo $gateway->id; ?>" <?php if ( ! $gateway->chosen ) : ?>style="display:none;"<?php endif; ?>>
+			<?php $gateway->payment_fields(); ?>
+		</div>
+	<?php endif; ?>
+</li>
 </form>
 <script>
     const base_url = '<?php echo get_site_url(); ?>';
