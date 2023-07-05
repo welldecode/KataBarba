bf3f3c77ff2bcd644e62d0d68bd820757fc60d12
welldecode <wellingtonalfredo550@gmail.com>
2023-07-05 13:51:15 -0300

    adicionado arquivos

--- page.php
+++ page.php
@@ -3,7 +3,7 @@ if ( have_posts() ) { while ( have_posts() ) { the_post(); ?>
 <div class="postI"> 
   	<?php the_content(); ?> 
 
-  <?php do_shortcode('[calculadora_melhor_envio product_id="product_id"]'); ?>
+  <?php do_shortcode('[calculadora_melhor_envio product_id="14"]'); ?>
   <div class="clear"></div>
 </div>
 <?php }}  get_footer(); ?>
