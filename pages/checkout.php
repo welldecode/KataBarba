6196bdddc6e7aa238ffd72792ae168ba8a5493aa
welldecode <wellingtonalfredo550@gmail.com>
2023-07-05 18:01:48 -0300

    adicionado arquivos

--- pages/checkout.php
+++ pages/checkout.php
@@ -151,42 +151,7 @@
 </main>
 
 <form id="order_review" method="post">
-
-	<table class="shop_table">
-		<thead>
-			<tr>
-				<th class="product-name"><?php _e( 'Product', 'woocommerce' ); ?></th>
-				<th class="product-quantity"><?php _e( 'Qty', 'woocommerce' ); ?></th>
-				<th class="product-total"><?php _e( 'Totals', 'woocommerce' ); ?></th>
-			</tr>
-		</thead>
-		<tbody>
-			<?php
-			if ( sizeof( $order->get_items() ) > 0 ) :
-				foreach ( $order->get_items() as $item ) :
-					echo '
-						<tr>
-							<td class="product-name">' . $item['name'].'</td>
-							<td class="product-quantity">' . $item['qty'].'</td>
-							<td class="product-subtotal">' . $order->get_formatted_line_subtotal( $item ) . '</td>
-						</tr>';
-				endforeach;
-			endif;
-			?>
-		</tbody>
-		<tfoot>
-		<?php
-			if ( $totals = $order->get_order_item_totals() ) foreach ( $totals as $total ) :
-				?>
-				<tr>
-					<th scope="row" colspan="2"><?php echo $total['label']; ?></th>
-					<td class="product-total"><?php echo $total['value']; ?></td>
-				</tr>
-				<?php
-			endforeach;
-		?>
-		</tfoot>
-	</table>
+ 
 
 	<div id="payment"> 
 		<ul class="payment_methods methods">
