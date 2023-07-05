be34aa279a8ad709d6d68427c0626188e8bc0ee1
welldecode <wellingtonalfredo550@gmail.com>
2023-07-05 17:59:19 -0300

    adicionado arquivos

--- page.php
+++ page.php
@@ -1,8 +1,102 @@
-<?php get_header(); 
-if ( have_posts() ) { while ( have_posts() ) { the_post(); ?>
-<div class="postI">  
-  	<?php the_content(); ?> 
+<?php
+/* Template Name: Finalizar Compra */
+?>
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+
+<head>
+  <meta charset="<?php bloginfo('charset'); ?>" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+  <title><?= site_title(); ?></title>
+
+  <!--- WP HEAD --->
+  <?php wp_head(); ?>
+
+</head>
+
+<main>
+  <section class="checkout">
+    <div class="checkout_content container">
+      <div class="checkout_woocommerce">
+        <div class="checkout_logo"><img src="<?= THEME_URI ?>/assets/img/icons/kata_logo.svg" alt=""></div>
+
  
-  <div class="clear"></div>
-</div>
-<?php }}  get_footer(); ?>
+        <?php the_content(); ?>
+      </div>
+      <div class="checkout_items">
+        <div class="checkout_items_content">
+
+          <?php
+          global $woocommerce;
+          $items = $woocommerce->cart->get_cart();
+
+          foreach ($items as $item => $values) {
+            $_product = wc_get_product($values['data']->get_id());
+          ?>
+
+            <div class="info_item">
+              <figure>
+                <?php
+                $getProductDetail = wc_get_product($values['product_id']);
+                echo $getProductDetail->get_image(); // accepts 2 arguments ( size, attr )
+                ?>
+                <div class="quantity">3</div>
+              </figure>
+              <div class="info">
+                <div class="info_title">
+                  <h2><?php echo $_product->get_title(); ?></h2>
+                  <span>Descrição</span>
+                </div>
+                <div class="sub_total">
+                  <span> R$ <?php echo WC()->cart->get_subtotal(); ?> </span>
+                </div>
+
+              </div>
+            </div>
+          <?php
+          }
+          ?>
+          <hr>
+
+          <div class="cupom_content">
+            <h1>Tem um cupom de desconto?</h1>
+            <div class="input_cupom">
+              <input type="text" placeholder="cartão-presente ou código de desconto">
+              <a href="#">Aplicar</a>
+            </div>
+          </div>
+
+          <hr>
+          <div class="price_total">
+            <div class="left_price">
+              <div class="col-1">
+                <span>SubTotal</span>
+                <span>R$ <?php echo WC()->cart->get_subtotal(); ?></span>
+              </div>
+              <div class="col-1">
+                <span>Frete</span>
+                <span>R$ <?php echo WC()->cart->get_subtotal_tax(); ?></span>
+              </div>
+            </div>
+          </div>
+          <hr>
+          <div class="total_order">
+            <?php echo WC()->cart->get_cart_subtotal(); ?>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+</main>
+
+<script>
+  const base_url = '<?php echo get_site_url(); ?>';
+</script>
+
+<?php wp_footer(); ?>
+
+</body>
+
+</html>
--- pages/checkout.php
+++ pages/checkout.php
@@ -149,7 +149,34 @@
         </div>
     </section>
 </main>
+<?php
+				if ( $available_gateways = WC()->payment_gateways->get_available_payment_gateways() ) {
+					// Chosen Method
+					if ( sizeof( $available_gateways ) ) {
+						current( $available_gateways )->set_current();
+					}
 
+					foreach ( $available_gateways as $gateway ) {
+						?>
+						<li class="payment_method_<?php echo $gateway->id; ?>">
+							<input id="payment_method_<?php echo $gateway->id; ?>" type="radio" class="input-radio" name="payment_method" value="<?php echo esc_attr( $gateway->id ); ?>" <?php checked( $gateway->chosen, true ); ?> data-order_button_text="<?php echo esc_attr( $gateway->order_button_text ); ?>" />
+							<label for="payment_method_<?php echo $gateway->id; ?>"><?php echo $gateway->get_title(); ?> <?php echo $gateway->get_icon(); ?></label>
+							<?php
+								if ( $gateway->has_fields() || $gateway->get_description() ) {
+									echo '<div class="payment_box payment_method_' . $gateway->id . '" style="display:none;">';
+									$gateway->payment_fields();
+									echo '</div>';
+								}
+							?>
+						</li>
+						<?php
+					}
+				} else {
+
+					echo '<p>' . __( 'Sorry, it seems that there are no available payment methods for your location. Please contact us if you require assistance or wish to make alternate arrangements.', 'woocommerce' ) . '</p>';
+
+				}
+			?>
 <script>
     const base_url = '<?php echo get_site_url(); ?>';
 </script>
