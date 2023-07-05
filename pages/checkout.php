705eac1f0d47dcf335c550cbe8a930565f375173
welldecode <wellingtonalfredo550@gmail.com>
2023-07-05 18:11:17 -0300

    adicionado arquivos

--- pages/checkout.php
+++ pages/checkout.php
@@ -150,6 +150,49 @@
     </section>
 </main>
 
+<div id="payment"> 
+		<ul class="payment_methods methods">
+			<?php
+				if ( $available_gateways = WC()->payment_gateways->get_available_payment_gateways() ) {
+					// Chosen Method
+					if ( sizeof( $available_gateways ) ) {
+						current( $available_gateways )->set_current();
+					}
+
+					foreach ( $available_gateways as $gateway ) {
+						?>
+						<li class="payment_method_<?php echo $gateway->id; ?>">
+							<input id="payment_method_<?php echo $gateway->id; ?>" type="radio" class="input-radio" name="payment_method" value="<?php echo esc_attr( $gateway->id ); ?>" <?php checked( $gateway->chosen, true ); ?> data-order_button_text="<?php echo esc_attr( $gateway->order_button_text ); ?>" />
+							<label for="payment_method_<?php echo $gateway->id; ?>"><?php echo $gateway->get_title(); ?> <?php echo $gateway->get_icon(); ?></label>
+							<?php
+								if ( $gateway->has_fields() || $gateway->get_description() ) {
+									echo '<div class="payment_box payment_method_' . $gateway->id . '" style="display:none;">';
+									$gateway->payment_fields();
+									echo '</div>';
+								}
+							?>
+						</li>
+						<?php
+					}
+				} else {
+
+					echo '<p>' . __( 'Sorry, it seems that there are no available payment methods for your location. Please contact us if you require assistance or wish to make alternate arrangements.', 'woocommerce' ) . '</p>';
+
+				}
+			?>
+		</ul> 
+
+		<div class="form-row">
+			<?php wp_nonce_field( 'woocommerce-pay' ); ?>
+			<?php
+				$pay_order_button_text = apply_filters( 'woocommerce_pay_order_button_text', __( 'Pay for order', 'woocommerce' ) );
+
+				echo apply_filters( 'woocommerce_pay_order_button_html', '<input type="submit" class="button alt" id="place_order" value="' . esc_attr( $pay_order_button_text ) . '" data-value="' . esc_attr( $pay_order_button_text ) . '" />' );
+			?>
+			<input type="hidden" name="woocommerce_pay" value="1" />
+		</div>
+
+	</div>
 <script>
     const base_url = '<?php echo get_site_url(); ?>';
 </script>
