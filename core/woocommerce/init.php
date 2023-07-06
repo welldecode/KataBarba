bbed3248acb694084f6fd9fe3d90a1462732128d
welldecode <wellingtonalfredo550@gmail.com>
2023-07-06 19:10:30 -0300

    adicionado arquivos

--- core/woocommerce/init.php
+++ core/woocommerce/init.php
@@ -22,10 +22,8 @@ function woocommerce_block_styles()
     wp_dequeue_style('wc-blocks-style');
     wp_dequeue_style('classic-theme-styles');
     wp_dequeue_style('wp-block-library'); // WordPress core
-    wp_dequeue_style('wp-block-library-theme'); // WordPress core
-    wp_dequeue_style('wc-block-style'); // WooCommerce
-    wp_dequeue_style('storefront-gutenberg-blocks'); // Storefront theme
-    wp_deregister_script('jquery');
+    wp_dequeue_style('wp-block-library-theme'); // WordPress core 
+    wp_dequeue_style('storefront-gutenberg-blocks'); // Storefront theme 
 }
 add_action('wp_enqueue_scripts', 'woocommerce_block_styles');
 function cart_script()
