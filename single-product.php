b11e846cfca59a31d158a7cad76477aed7171a25
welldecode <wellingtonalfredo550@gmail.com>
2023-07-06 20:46:24 -0300

    adicionado arquivos

--- single-product.php
+++ single-product.php
@@ -4,14 +4,10 @@
      <div class="container">
          <div class="row">
  
-
-             <div id="main" class="<?php echo 'full_width_layout' == $presentation_options['layout'] || get_post_meta(get_the_ID(), 'standard_seo_post_level_layout', true) ? 'span12 fullwidth' : 'span8'; ?> clearfix" role="main">
  
 
                  <?php woocommerce_single_product_content(); ?>
- 
-             </div><!-- /#main -->
- 
+  
 
          </div> <!-- /row -->
      </div><!-- /container -->
