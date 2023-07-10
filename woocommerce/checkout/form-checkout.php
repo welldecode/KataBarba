026e02cc0e7b947af761bd0c068185ff5ef80cc5
welldecode <wellingtonalfredo550@gmail.com>
2023-07-10 16:09:46 -0300

    adicionado arquivos

--- woocommerce/checkout/form-checkout.php
+++ woocommerce/checkout/form-checkout.php
@@ -38,22 +38,53 @@ if (!$checkout->is_registration_enabled() && $checkout->is_registration_required
             <div class="card px-0 pt-4 pb-0 mt-3 mb-3">
                 <h2 id="heading">Sign Up Your User Account</h2>
                 <p>Fill all form field to go to next step</p>
-                <form id="msform">
+
+                <form name="checkout" method="post" class="checkout woocommerce-checkout" action="<?php echo esc_url(wc_get_checkout_url()); ?>" enctype="multipart/form-data">
                     <!-- progressbar -->
                     <ul id="progressbar">
                         <li class="active" id="billing"><strong>Cobrança Envio</strong></li>
-                        <li id="payment"><strong>Pagamento</strong></li> 
+                        <li id="payment"><strong>Pagamento</strong></li>
                     </ul>
                     <div class="progress">
                         <div class="progress-bar progress-bar-striped progress-bar-animated" role="progressbar" aria-valuemin="0" aria-valuemax="100"></div>
                     </div> <br> <!-- fieldsets -->
                     <fieldset>
+                        <div class="col-xs-12 col-sm-12 col-md-7 col-woo-checkout-details">
+                            <?php if ($checkout->get_checkout_fields()) : ?>
+
+                                <?php do_action('woocommerce_checkout_before_customer_details'); ?>
+
+                                <div id="customer_details">
+                                    <div class="checkout-billing">
+                                        <?php do_action('woocommerce_checkout_billing'); ?>
+                                    </div>
+
+                                    <div class="checkout-shipping">
+                                        <?php do_action('woocommerce_checkout_shipping'); ?>
+                                    </div>
+                                </div>
+
+                                <?php do_action('woocommerce_checkout_after_customer_details'); ?>
+
+                            <?php endif; ?>
+                        </div>
                         <input type="button" name="next" class="next action-button" value="Next" />
                     </fieldset>
                     <fieldset>
+                        <div class="col-xs-12 col-sm-12 col-md-5">
+                            <h3 id="order_review_heading"><?php esc_html_e('Your order', 'martfury'); ?></h3>
+
+                            <?php do_action('woocommerce_checkout_before_order_review'); ?>
+
+                            <div id="order_review" class="woocommerce-checkout-review-order">
+                                <?php do_action('woocommerce_checkout_order_review'); ?>
+                            </div>
+
+                            <?php do_action('woocommerce_checkout_after_order_review'); ?>
+                        </div>
                         <input type="button" name="next" class="next action-button" value="Next" /> <input type="button" name="previous" class="previous action-button-previous" value="Previous" />
                     </fieldset>
-                 
+
                     <fieldset>
                         <div class="form-card">
                             <div class="row">
@@ -67,45 +98,12 @@ if (!$checkout->is_registration_enabled() && $checkout->is_registration_required
 
                         </div>
                     </fieldset>
+
                 </form>
+
             </div>
         </div>
     </div>
 </div>
-<form name="checkout" method="post" class="checkout woocommerce-checkout" action="<?php echo esc_url(wc_get_checkout_url()); ?>" enctype="multipart/form-data">
-    <div class="row">
-        <div class="col-xs-12 col-sm-12 col-md-7 col-woo-checkout-details">
-            <?php if ($checkout->get_checkout_fields()) : ?>
-
-                <?php do_action('woocommerce_checkout_before_customer_details'); ?>
-
-                <div id="customer_details">
-                    <div class="checkout-billing">
-                        <?php do_action('woocommerce_checkout_billing'); ?>
-                    </div>
-
-                    <div class="checkout-shipping">
-                        <?php do_action('woocommerce_checkout_shipping'); ?>
-                    </div>
-                </div>
-
-                <?php do_action('woocommerce_checkout_after_customer_details'); ?>
-
-            <?php endif; ?>
-        </div>
-        <div class="col-xs-12 col-sm-12 col-md-5">
-            <h3 id="order_review_heading"><?php esc_html_e('Your order', 'martfury'); ?></h3>
-
-            <?php do_action('woocommerce_checkout_before_order_review'); ?>
-
-            <div id="order_review" class="woocommerce-checkout-review-order">
-                <?php do_action('woocommerce_checkout_order_review'); ?>
-            </div>
-
-            <?php do_action('woocommerce_checkout_after_order_review'); ?>
-        </div>
-    </div>
-
-</form>
 
 <?php do_action('woocommerce_after_checkout_form', $checkout); ?>
