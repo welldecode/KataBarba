d3f0b64946940af74ed34d7998f4d63d88fe2299
welldecode <wellingtonalfredo550@gmail.com>
2023-07-10 16:20:27 -0300

    adicionado arquivos

--- woocommerce/checkout/form-checkout.php
+++ woocommerce/checkout/form-checkout.php
@@ -31,7 +31,7 @@ if (!$checkout->is_registration_enabled() && $checkout->is_registration_required
     return;
 }
 
-?>asdadds
+?> 
 <div class="container-fluid">
     <div class="row justify-content-center">
         <div class="col-11 col-sm-10 col-md-10 col-lg-6 col-xl-5 text-center p-0 mt-3 mb-2">
@@ -84,20 +84,7 @@ if (!$checkout->is_registration_enabled() && $checkout->is_registration_required
                         </div>
                         <input type="button" name="next" class="next action-button" value="Next" /> <input type="button" name="previous" class="previous action-button-previous" value="Previous" />
                     </fieldset>
-
-                    <fieldset>
-                        <div class="form-card">
-                            <div class="row">
-                                <div class="col-7">
-                                    <h2 class="fs-title">Finish:</h2>
-                                </div>
-                                <div class="col-5">
-                                    <h2 class="steps">Step 4 - 4</h2>
-                                </div>
-                            </div>
-
-                        </div>
-                    </fieldset>  
+ 
 
                 </form>
                  
